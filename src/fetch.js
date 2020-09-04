c12010951d25e6e6705d0e77faeeecf31f987fe5
amitdesh <amitdesh2014@gmail.com>
2020-09-04 13:24:02 -0400

    Basic API calls

--- src/fetch.js
+++ src/fetch.js
@@ -1,7 +1,14 @@
 class FetchAdapter {
+    constructor(searchBy, searchWords){
+        this.searchBy = searchBy
+        this.searchWords = searchWords.split(' ').join('+')
+        this.baseURL = `http://openlibrary.org/search.json?${this.searchBy}=${this.searchWords}&limit=5`
+    }
 
-
-
-
+    fetch(){
+        return fetch(this.baseURL)
+        .then(resp => resp.json())
+        .then(console.log)
+    }
 
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ console.log("Hello")
 
 document.addEventListener("DOMContentLoaded", e=>{
 
-
-
-
-
+    document.addEventListener("submit", e=>{
+        e.preventDefault()
+        if (e.target.id === 'search-form'){
+            let form = e.target
+            let searchWords = form['search-q'].value
+            console.log(searchWords)
+            let searchBy = form['search-method'].value
+            console.log(searchBy)
+            let search = new FetchAdapter(searchBy, searchWords)
+            console.log(search.fetch())
+            // search.fetch()
+            
+        }
+    })
 })
+
 
 
